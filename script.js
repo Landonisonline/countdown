0a1822d80825e3224966f89912b8f862ccc9cf19
landonisonline <53879728+Landonisonline@users.noreply.github.com>
2020-05-02 23:41:06 -0500

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 24, 2020 03:51:00").getTime();
+var countDownDate = new Date("May 2, 2020 23:40:00").getTime();
 
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
   // Get today's date and time
   var now = new Date().getTime();
-  
+
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
@@ -20,7 +20,7 @@ var x = setInterval(function() {
 
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = "Expires in: " + hours + " hours "
-  + minutes + " minutes " + seconds + " seconds ";
+    + minutes + " minutes " + seconds + " seconds ";
 
   // If the count down is finished, write some text 
   if (distance < 0) {
